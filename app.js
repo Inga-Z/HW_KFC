591b3382d7eef396d117a24788226a61f689d7f0
Inga-Z <z_inga@mail.ru>
2019-10-08 22:26:09 +0300

    Предитог

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 console.log('worker');
+
+const food = {
+    id: 'kfc',
+    avatarUrl: 'https://placekitten.com/50/50',
+};
+
+const first = { 
+    id: 'photo-1',
+    price: 124,
+    photo: 'https://placekitten.com/50/50',
+    name: 'Чизбургер Де Люкс',
+    quantity: 1,
+    summ: 124,
+    weight: 226,
+    content: 'Большой контент!!!',
+    calories: 193,
+    squirrels: 12.6,
+    fats: 6.6,
+    carbohydrates: 20.7,
+};
+
+console.log(first); 
+
+const second = { 
+    id: 'photo-2',
+    price: 79,
+    photo: 'https://placekitten.com/50/50',
+    name: 'Хот-дог куриный',
+    quantity: 1,
+    summ: 79,
+    weight: 124,
+    content: 'Большой контент!!!',
+    calories: 226,
+    squirrels: 9.6,
+    fats: 10.1,
+    carbohydrates: 24.3,
+};
+
+console.log(second); 
