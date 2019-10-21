de571f910ed3763912ba4f755ea8f2530bb3754a
Inga-Z <z_inga@mail.ru>
2019-10-21 21:36:11 +0300

    Итог

--- app.js
+++ app.js
@@ -1,37 +1,24 @@
 console.log('worked');
 
 
-const burger = {
-    author: 'burger',
-    name: 'Чизбургер Де Люкс',
-    avatarUrl: 'https://s82079.cdn.ngenix.net/MXahUbKht6Q6D2wGRYrAz7nu.png?dw=230',
-
-};
-
 const firstBurger = {
-    author: burger,
-    name: 'Хот-дог куриный',
+    name: 'Хот-дог',
     imgUrl: 'https://s82079.cdn.ngenix.net/Wyvkk6X2vsQ7jGr8c84MzfK1.png?dw=230',
 };
 
 const burgerFerstEl = document.getElementById('photo-1'); 
-burgerFerstEl.src = firstBurger.author.avatarUrl;
+burgerFerstEl.src = firstBurger.imgUrl;
 
 const burgerFerstNameEl = document.getElementById('author-name1');
-burgerFerstNameEl.textContent = firstBurger.author.name;
+burgerFerstNameEl.textContent = firstBurger.name;
 
 const secondBurger = {
-    author: burger,
     name: 'Чизбургер Де Люкс',
     imgUrl: 'https://s82079.cdn.ngenix.net/MXahUbKht6Q6D2wGRYrAz7nu.png?dw=230',
 };
 
-const burgerSecondEl = document.getElementById('photo-2'); 
-burgerSecondEl.src = secondBurger.author.avatarUrl;
+const burgerSecondtEl = document.getElementById('photo-2'); 
+burgerSecondtEl.src = secondBurger.imgUrl;
 
 const burgerSecondNameEl = document.getElementById('author-name2');
-burgerSecondNameEl.textContent = secondBurger.author.name;
-
-
-
-
+burgerSecondNameEl.textContent = secondBurger.name;
